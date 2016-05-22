6d113fdc516cf1cc433a77016001c01013d19863
lxyea <larrylanxiangye@aim.com>
2016-05-23 01:36:08 +1000

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,11 @@
+#load and filter data - can use plot1 code/data
+fn <- "./data/household_power_consumption.txt"
+dt <- read.table(fn, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsdt <- dt[dt$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+datetime <- strptime(paste(subsdt$Date, subsdt$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subsdt$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
